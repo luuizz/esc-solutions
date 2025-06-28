3c9f4fbc1bb7111216c3ccc086951e49070e0b7e
Luiz Ricardo <luu.izz@hotmail.com>
2025-06-28 19:55:39 -0300

    feat: criado arquivo geral de dados

---
+++ template-parts/data.php
@@ -0,0 +1,64 @@
+<?php
+$social_media = [
+  [
+    'name' => 'Linkedin',
+    'link' => '#',
+    'icon' => 'icon-linkedin'
+  ],
+  [
+    'name' => 'Instagram',
+    'link' => '#',
+    'icon' => 'icon-instagram'
+  ],
+  [
+    'name' => 'Facebook',
+    'link' => '#',
+    'icon' => 'icon-facebook'
+  ],
+  [
+    'name' => 'Twitter',
+    'link' => '#',
+    'icon' => 'icon-twitter'
+  ],
+  [
+    'name' => 'Youtube',
+    'link' => '#',
+    'icon' => 'icon-youtube'
+  ]
+];
+
+$footer_links = [
+  [
+    'column' => 'Conheça',
+    'links' => [
+      [ 'name' => 'Como funciona', 'link' => '#' ],
+      [ 'name' => 'Soluções', 'link' => '#' ],
+      [ 'name' => 'Planos de adesão', 'link' => '#' ],
+    ]
+  ],
+  [
+    'column' => 'Conteúdo',
+    'links' => [
+      [ 'name' => 'Notícias', 'link' => '#' ],
+      [ 'name' => 'Artigos', 'link' => '#' ],
+      [ 'name' => 'Política de Privacidade', 'link' => '#' ],
+    ]
+  ],
+  [
+    'column' => 'Atendimento',
+    'links' => [
+      [
+        'name' => '(61) 98158-4546',
+        'link' => 'https://api.whatsapp.com/send?phone=5561981584546&text=Ol%C3%A1,%20gostaria%20de%20fazer%20um%20or%C3%A7amento.',
+        'icon' => 'icon-whatsapp-line'
+      ],
+      [
+        'name' => 'faleconosco@emprestalegal.com.br',
+        'link' => 'mailto:faleconosco@emprestalegal.com.br',
+      ],
+    ]
+  ]
+];
+
+
+?>
