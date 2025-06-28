993b164ca626f1ac2f63b00b0acf4819d4854350
Luiz Ricardo <luu.izz@hotmail.com>
2025-06-28 19:57:51 -0300

    feat: criado html e feito responsivo parcial

--- header.php
+++ header.php
@@ -9,6 +9,29 @@
 </head>
 <body <?php body_class(); ?>>
 
+<header>
+  <div class="container">
+    <a href="<?php echo home_url(); ?>" class="logo" title="Ir para página Inicial">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.webp" alt="Logo da ESC Solutions">
+    </a>
 
-<h1>Header</h1>
+    <nav class="navigation">
+      <ul class="links">
+        <li><a href="#servicos">Procuro crédito</a></li>
+        <li><a href="#sobre">Quero fornecer crédito</a></li>
+        <li><a href="#solucoes">Soluções</a></li>
+        <li><a href="#planos">Planos</a></li>
+        <li><a href="#">Fale conosco</a></li>
+      </ul>
 
+      <a href="#" class="btn secondary small with-icon">
+        <span>Login</span>
+        <?= render_svg_icon('user', 'icon-user') ?>
+      </a>
+
+      <button id="js-btn-menu" class="btn-menu btn secondary small with-icon" tabindex="0" aria-label="Abrir Menu" role="button" aria-controls="navigation">
+        <span class="btn-mobile-inner"></span>
+      </button>
+    </nav>
+  </div>
+</header>
