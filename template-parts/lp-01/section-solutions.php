781f0eb77e50e45f4b87af04c7b2829b4b01b8ea
Luiz Ricardo <luu.izz@hotmail.com>
2025-06-29 13:04:14 -0300

    feat: criado a primeira parte da seção

--- template-parts/lp-01/section-solutions.php
+++ template-parts/lp-01/section-solutions.php
@@ -1,5 +1,14 @@
-<section>
+<section class="s-solutions">
   <div class="container">
-    <h2>Soluções</h2>
+    <div class="top-area">
+      <?php 
+      require_once __DIR__ . '/../components/caption.php';
+      echo render_caption('Nossas soluções'); 
+      ?>
+      <h2>Tecnologia, inteligência e assessoria para sua ESC crescer</h2>
+      <p>Mais do que abrir uma ESC, oferecemos um ecossistema completo para você operar com eficiência, segurança e escalabilidade.</p>
+    </div>
+
+    <a href="#" class="btn primary">Abrir minha ESC</a>
   </div>
 </section>
