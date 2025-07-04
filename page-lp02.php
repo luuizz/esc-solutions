f19b86e808037cdd117911fdee3d56e49cee22ff
Luiz Ricardo <luu.izz@hotmail.com>
2025-07-04 20:23:12 -0300

    feat: inserido componentes e seções

---
+++ page-lp02.php
@@ -0,0 +1,21 @@
+<?php
+//Template Name: Landing Page 02
+
+get_header();
+?>
+
+<?php require_once 'template-parts/lp-02/section-header.php'; ?>
+
+<?php require_once 'template-parts/lp-02/section-system.php'; ?>
+
+<?php require_once 'template-parts/lp-02/section-features.php'; ?>
+
+<?php require_once 'template-parts/lp-02/section-video.php'; ?>
+
+<?php require_once 'template-parts/components/blog.php'; ?>
+
+<?php require_once 'template-parts/components/magazine.php'; ?>
+
+<?php
+get_footer();
+?>
