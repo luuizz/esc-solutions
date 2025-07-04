448324abbda4346c77ad502edfc02f3d6dc1dfee
Luiz Ricardo <luu.izz@hotmail.com>
2025-07-04 20:24:48 -0300

    feat: criado lógica para modal em geral

---
+++ js/scripts/modal.js
@@ -0,0 +1,41 @@
+document.querySelectorAll('.js-btn-modal').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const modalId = btn.dataset.modalId;
+    const modal = document.getElementById(modalId);
+    modal?.classList.add('show');
+  });
+});
+
+document.querySelectorAll('.js-btn-close').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const modal = btn.closest('.modal');
+    modal?.classList.remove('show');
+  });
+});
+
+// Fecha o modal ao clicar no fundo
+document.querySelectorAll('.modal-overlay').forEach((overlay) => {
+  overlay.addEventListener('click', (e) => {
+    if (e.target === overlay) {
+      overlay.closest('.modal')?.classList.remove('show');
+    }
+  });
+});
+
+//Download da revista ao preencher o formulário
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('#wpforms-form-29');
+  if (form) {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      const pdfUrl =
+        'http://esc-solutions.local/wp-content/uploads/2025/07/1711976423145Plano20de20AC3A7C3A3o20para20Contruir20Futuros20de20Grandes20Empresas.pdf';
+      const link = document.createElement('a');
+      link.href = pdfUrl;
+      link.download = 'YourCustomPDF.pdf'; //
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  }
+});
