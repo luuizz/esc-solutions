bf329f5366cb11ab4b37415dae3210d53a34ad72
Luiz Ricardo <luu.izz@hotmail.com>
2025-07-04 20:22:59 -0300

    fix: nome alterado de home para lp

---
+++ page-lp01.php
@@ -0,0 +1,23 @@
+<?php 
+//Template Name: Landing Page 01
+get_header();
+
+?>
+
+<?php require_once 'template-parts/lp-01/section-hero.php'; ?>
+
+<?php require_once 'template-parts/lp-01/section-how-work.php'; ?>
+
+<?php require_once 'template-parts/lp-01/section-solutions.php'; ?>
+
+<?php require_once 'template-parts/lp-01/section-plans.php'; ?>
+
+<?php require_once 'template-parts/components/blog.php'; ?>
+
+<?php require_once 'template-parts/components/magazine.php'; ?>
+
+<?php get_template_part('template-parts/components/modal-lp01') ?>
+
+<?php get_template_part('template-parts/components/modal-video') ?>
+
+<?php get_footer(); ?>
