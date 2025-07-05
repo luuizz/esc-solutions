51d42ef329c553cf7bb096430a344e026149e882
Luiz Ricardo <luu.izz@hotmail.com>
2025-07-05 16:57:03 -0300

    feat: criado html

---
+++ template-parts/components/form.php
@@ -0,0 +1,17 @@
+<form action="">
+  <div class="mail-data select-wrapper">
+    <select name="" id="">
+      <option value="">Já possuo ESC</option>
+      <option value="">Não possuo ESC e desejo abrir</option>
+      </select>
+  </div>
+  <div class="mail-data">
+    <input type="text" id="cnpj" name="cnpj" placeholder="CNPJ" maxlength="18" required>
+  </div>
+  <div class="mail-data">
+    <input type="email" placeholder="Insira seu e-mail">
+  </div>
+  <div class="form-button">
+    <button class="btn primary" type="submit">Comece agora</button>
+  </div>
+</form>
