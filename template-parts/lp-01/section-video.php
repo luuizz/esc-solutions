cef3931ac71e7a74a97a065307f38f72642692e9
Luiz Ricardo <luu.izz@hotmail.com>
2025-08-18 23:26:05 -0300

    feat: criado a seção do vídeo para a lp01

---
+++ template-parts/lp-01/section-video.php
@@ -0,0 +1,15 @@
+<?php
+$imagem = get_field('_poster_video_1', 'option');
+?>
+
+<section class="s-video js-btn-modal" data-modal-id="modal-video-01">
+  <div class="poster" id="js-video-poster">
+    <img src="<?php echo esc_url($imagem['url']); ?>" alt="<?php echo esc_attr($imagem['alt']); ?>" title="<?php echo esc_attr($imagem['title']); ?>">
+    <button class="btn-play js-btn-modal" data-modal-id="modal-video">
+      <span class="sr-only">Reproduzir video</span>
+      <div class="wrapper">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/icon-play-mini-fill-lp02.svg" alt="Ícone de Play">
+      </div>
+    </button>
+  </div>
+</section>
